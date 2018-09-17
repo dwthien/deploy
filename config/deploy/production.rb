db2de1f633bdc4a019e5c075ee91e82de6d253a8
Thien <thien.nguyen@dirox.net>
2018-09-17 17:42:05 +0700

    test 2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :rails_env, :production
 set :deploy_to, "/home/thien/project/deploy"
-server "207.246.125.18", user: "thien", roles: %w(web app db)
+server "207.246.125.18", user: "thien", roles: %w(web app db),  primary: true
